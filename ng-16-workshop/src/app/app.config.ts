6112b0d0e516b97d8724ea2b20a490caae54da67
Kingsley Owusu-Sekyere <k.owususekyere.86@gmail.com>
2023-11-28 14:10:30 +0100

    Add ngrx
    ng add @ngrx/store@latest
    ng add @ngrx/store-devtools@latest

--- ng-16-workshop/src/app/app.config.ts
+++ ng-16-workshop/src/app/app.config.ts
@@ -2,6 +2,8 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { mockBackendInterceptor } from '@score/api';
 import { routes } from './app.routes';
 
@@ -9,5 +11,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(withInterceptors([mockBackendInterceptor])),
+    provideStoreDevtools(),
+    provideStore(),
   ],
 };
